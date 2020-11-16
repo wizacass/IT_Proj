ef70cf2938651e10618259c8d70e0f0af32fed0a
Vytenis Petrauskas <visak.pet0@gmail.com>
2020-11-16 18:52:20 +0200

    Create basic migrations

---
+++ database/migrations/2020_11_16_153135_create_plane_parts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanePartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plane_parts', function (Blueprint $table) {
+            $table->id();
+            $table->string('manufacturer');
+            $table->string('model');
+            $table->decimal('price');
+            $table->unsignedInteger('amount');
+            $table->dateTime('delivery_time');
+            $table->string('part_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plane_parts');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RoleSeeder::class);
+        $this->call([
+            RoleSeeder::class,
+            DeliveryStatusSeeder::class,
+        ]);
     }
 }
