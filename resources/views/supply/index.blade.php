a65048304ef3345dbaed7d150e52b50c1695e8be
Vytenis Petrauskas <visak.pet0@gmail.com>
2020-11-17 00:29:40 +0200

    Create and implement component for card

--- resources/views/supply/index.blade.php
+++ resources/views/supply/index.blade.php
@@ -2,38 +2,10 @@
 
 @section('content')
     <x-hero subtitle="{{ $user->email }}, {{ $user->role->name }}">Welcome back!</x-hero>
-
     <div class="container" style="padding-top: 2em">
         <div class="tile is-ancestor has-text-centered">
-            <div class="tile is-parent">
-                <div class="tile is-child card">
-                    <div class="card-content has-background-primary-light">
-                        <span class="icon is-large">
-                            <i class="fas fa-3x fa-plane"></i>
-                        </span>
-                    </div>
-                    <footer class="card-footer has-background-primary">
-                        <div class="container" style="padding: 1em">
-                            <a class="title" href="#">View Parts</a>
-                        </div>
-                    </footer>
-                </div>
-            </div>
-            <div class="tile is-parent">
-                <div class="tile is-child card">
-                    <div class="card-content has-background-primary-light">
-                        <span class="icon is-large">
-                            <i class="fas fa-3x fa-globe-europe"></i>
-                        </span>
-                    </div>
-                    <footer class="card-footer has-background-primary">
-                        <div class="container" style="padding: 1em">
-                            <a class="title" href="#">View Orders</a>
-                        </div>
-                    </footer>
-                </div>
-            </div>
+            <x-iconcard icon="fa-plane" subtitle="View Parts" href="#"/>
+            <x-iconcard icon="fa-globe-europe" subtitle="View Orders" href="#"/>
         </div>
     </div>
-
 @endsection
