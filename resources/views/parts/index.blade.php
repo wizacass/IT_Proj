e810342dc57895eb69a9b774f6fa3c43b1f0a0a4
Vytenis Petrauskas <visak.pet0@gmail.com>
2020-11-17 00:42:37 +0200

    Register parts controller

---
+++ resources/views/parts/index.blade.php
@@ -0,0 +1,1 @@
+<p>Parts list</p>
--- resources/views/supply/index.blade.php
+++ resources/views/supply/index.blade.php
@@ -4,7 +4,7 @@
     <x-hero subtitle='{{ $user->email }}, "{{ $user->supplier->name }}" {{ $user->role->name }}'>Welcome back!</x-hero>
     <div class="container" style="padding-top: 2em">
         <div class="tile is-ancestor has-text-centered">
-            <x-iconcard icon="fa-plane" subtitle="View Parts" href="#"/>
+            <x-iconcard icon="fa-plane" subtitle="View Parts" href="/parts"/>
             <x-iconcard icon="fa-globe-europe" subtitle="View Orders" href="#"/>
         </div>
     </div>
