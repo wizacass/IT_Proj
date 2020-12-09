356011584b3dd162501dec4b179aeb251d2b7885
Vytenis Petrauskas <visak.pet0@gmail.com>
2020-12-09 21:22:50 +0200

    Replace order form inputs with Vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,24 +2,12 @@ window.Vue = require('vue');
 window.axios = require('axios');
 
 Vue.component('mainmenu', require('./components/menu.vue').default);
+Vue.component("orderinputs", require("./components/orderform.vue").default);
 
 const menuApp = new Vue({
     el: '#mainMenu',
 });
 
-const dynamicInputs = new Vue({
+const ordersApp = new Vue({
     el: '#orderForm',
-    data: {
-        count: 0,
-    },
-    methods: {
-        add: function() {
-            this.count++
-        },
-        remove: function() {
-            if(this.count > 0) {
-                this.count--
-            }
-        }
-    }
 });
--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -7,46 +7,11 @@
             @csrf
 
             <x-formerror />
-
-            <div class="container has-text-centered" style="padding-bottom: 1em">
-                <a class="button is-link is-inverted" v-on:click="add">
-                    <span class="icon is-small">
-                        <i class="fas fa-plus"></i>
-                    </span>
-                    <span>Add a new part</span>
-                </a>
-                <a class="button is-danger is-inverted" v-on:click="remove" v-if="count > 0">
-                    <span class="icon is-small">
-                        <i class="fas fa-minus"></i>
-                    </span>
-                    <span>Remove</span>
-                </a>
-            </div>
-
-            <div class="box" v-if="count > 0">
-                <div class="level" v-for="i in count">
-                    <div class="level-item">
-                        <p><b>@{{ i }}</b></p>
-                    </div>
-                    <div class="level-item">
-                        <div class="select is-fullwidth">
-                            <select name=parts[]>
-                                <option>Select a part</option>
-                                @foreach ($parts as $part)
-                                    <option value="{{ $part->id }}">[{{ $part->supplier->name }}] {{ $part->part_type }}
-                                        {{ $part->model }} </option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <div class="level-item">
-                        <x-inlineinput for="part_counts[]" type="number" placeholder="Amount" />
-                    </div>
-                </div>
-            </div>
+            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+            <orderinputs :products='@json($parts)'></orderinputs>
 
             <div class="field has-text-centered">
-                <button class="button is-primary is-rounded is-large" type="submit">Next</button>
+                <button class="button is-primary is-rounded is-large" type="submit">Order</button>
             </div>
         </form>
     </div>
