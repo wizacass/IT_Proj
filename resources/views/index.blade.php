e081517d697ddb20f36758cb4713d7ef71cc189a
Vytenis Petrauskas <visak.pet0@gmail.com>
2020-12-10 02:28:46 +0200

    Create landing page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h1 class="title">
+                    IT Proj. 2ag_a
+                </h1>
+                <h2 class="subtitle">
+                    Vytenis Petrauskas IFF-8-1
+                </h2>
+            </div>
+        </div>
+    </section>
+@endsection
